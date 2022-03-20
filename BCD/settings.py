b1acee33bfae83e02fc665721f0c54f35f5597cc
Pranjal Tulsiyan <pranjaltulsiyan1@gmail.com>
2022-03-20 14:34:12 +0530

    header js not working

--- BCD/settings.py
+++ BCD/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,  'BCD/static/'), ]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,  'BCD/static/'), ]
 
 STATIC_URL = '/static/'
 
