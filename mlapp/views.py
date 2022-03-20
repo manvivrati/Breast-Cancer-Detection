781e8d582062ab20889dd8e88a8963b74a892f9a
manvivrati <manvivrati@gmail.com>
2022-03-20 23:43:01 +0530

    navbar corrected

--- mlapp/views.py
+++ mlapp/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def printval(request):
     txt = "<h1 style='color:blue; text-align:center'>" \
-          "Welcome to the world! </h1>"
+          "Welcome to the Project for Breast Cancer Detection Using ML!\n Please go the index page! </h1>"
     return HttpResponse(txt)
 
 
